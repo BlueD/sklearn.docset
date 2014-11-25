bee693710a2ec7704486f86c9577cf4a3f36027f
BlueD <BlueDzzy@hotmail.com>
2014-11-25 15:39:50 +0800

    using miniconda

---
+++ mkhtml.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+deactivate
+
+wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh
+bash ./miniconda.sh
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
+conda install --yes mkl
+
+conda install --yes numpy scipy matplotlib scikit-learn
+
+mkdir cache
+pip install --download=cache/ scikit-learn
+ls -l cache/
